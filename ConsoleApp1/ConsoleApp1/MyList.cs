ead877a3ba56f120b46b77d9589ff33524367939
aslan <aslanqaribov7@gmail.com>
2023-04-12 13:50:14 +0400

    task

---
+++ ConsoleApp1/ConsoleApp1/MyList.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Mylist<T>
+        where T: class
+    {
+        public T[] _arr;
+        private static int _count;
+
+        public Mylist()
+        {
+            _arr = new T[0];
+            _count = 0;
+            _count++;
+        }
+        public T this[int i]
+        {
+            get { return _arr[i]; }
+            set { _arr[i] = value; }
+        }
+        public static int Count { get { return _count; } }
+
+        public void Add(T value)
+        {
+            Array.Resize(ref _arr, _arr.Length+1);
+            _arr[_arr.Length-1] = value;
+        }
+        public void Clear()
+        {
+            Array.Clear(_arr, 0, _arr.Length);
+            _count = 0;
+        }
+        public bool Exist(T value)
+        {
+            for (int i = 0; i< _arr.Length; i++)
+            {
+                if (_arr[i].Equals(value))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        public void Remove(T value)
+        {
+            T[] newarray = new T[0];
+
+            foreach (var items in _arr)
+            {
+                if (!items.Equals(value))
+                {
+                    Array.Resize(ref newarray, newarray.Length + 1);
+                    newarray[newarray.Length - 1] = items;
+                }
+            }
+
+            _arr = newarray;
+        }
+    }
+}
+
+
+
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> Customlist = new List<int>();
+            Customlist.Add(1);
+            Customlist.Clear();
+            Customlist.Remove(1);
+            Customlist.Count();
+        }
+    }
+}
